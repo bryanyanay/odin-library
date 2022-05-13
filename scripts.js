b526cd63ddcf37368cc024b4dd9774f0e00e6c71
Bryan Yan <tyrone.yan@hotmail.com>
2022-05-13 19:49:03 -0400

    Add basic files and Book constructor

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+Book.prototype.info = function() {
+    return `${this.title}, by ${this.author}, ${pages}, ${(read)?"already read":"not read yet"}`;
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+console.log(theHobbit.info());
