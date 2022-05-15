008b4c729c7a9c5326fc2364c560570428a4219d
Bryan Yan <tyrone.yan@hotmail.com>
2022-05-14 21:43:08 -0400

    Implement add book modal opening and closing

--- scripts.js
+++ scripts.js
@@ -10,5 +10,29 @@ Book.prototype.info = function() {
     return `${this.title}, by ${this.author}, ${this.pages}, ${(this.read)?"already read":"not read yet"}`;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+const obscure = document.getElementsByClassName("obscure")[0];
+const addBookBtn = document.getElementsByClassName("add-book-btn")[0];
+const addBookModal = document.getElementsByClassName("add-book-modal")[0];
+
+console.log(addBookBtn);
+
+function showAddBookModal() {
+    obscure.classList.add("showing");
+    addBookModal.classList.add("showing");
+}
+function hideAddBookModal() {
+    obscure.classList.remove("showing");
+    addBookModal.classList.remove("showing");
+}
+
+addBookBtn.onclick = showAddBookModal;
+obscure.onclick = () => {
+    if (obscure.classList.contains("showing")) {
+        hideAddBookModal();
+    }
+};
+
+
+
+
+
